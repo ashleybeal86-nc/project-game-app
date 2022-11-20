8a29f247d554994799b6b9d2caa671037764816d
Ashley Beal <ashleybeal86@yahoo.com>
2022-11-20 12:01:28 -0500

    added NavBar file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,39 +5,19 @@ import AboutUs from './Components/Pages/AboutUs';
 import ContactUs from './Components/Pages/ContactUs';
 import LoginForm from './Components/Pages/LoginForm';
 import SignUpForm from './Components/Pages/SignUpForm';
-
+import NavBar from './Components/Pages/NavBar';
 
 function App(){
 return (
   <div className="App">
-    <header>
       <h1 className="title">Our Game Arcade</h1>
-
-      <div className="navBar">
-        <ul>
-          <li>
-            <a href={<HomePage />}>Home</a>
-          </li>
-          <li>
-            <a href={<AboutUs />}>About Us</a>
-          </li>
-          <li>
-            <a href={<ContactUs />}>Contact Us</a>
-          </li>
-          <li>
-            <a href={<LoginForm />}>Login</a>
-          </li>
-          <li>
-            <a href={<SignUpForm />}>Sign Up</a>
-          </li>
-          <li>
-            <a href={<Games />}>Our Games</a>
-          </li>
-        </ul>
-      </div>
-    </header>
-  </div>
-);
+      <Home />
+      <Login />
+      <SignUp />
+      <ContactUs />
+      <AboutUs />
+    </div>
+  );
 }
 
 export default App;
